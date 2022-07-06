2a2c935216bc1abc6bb3ba9a48b02ba156785e2b
Your Name <you@example.com>
2022-07-06 15:28:22 -0400

    updating users.js

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,8 @@ const jsonschema = require("jsonschema");
 
 const express = require("express");
 const { ensureLoggedIn } = require("../middleware/auth");
+const {ensureAdmin } = require("../middleware/admin");
+const { ensureCorrectUserOrAdmin } = require("../middleware/ensureCorrectUserOrAdmin");
 const { BadRequestError } = require("../expressError");
 const User = require("../models/user");
 const { createToken } = require("../helpers/tokens");
@@ -26,8 +28,10 @@ const router = express.Router();
  *
  * Authorization required: login
  **/
+//Creating users should only permitted by admins (registration, however, should remain open 
+//to everyone).
 
-router.post("/", ensureLoggedIn, async function (req, res, next) {
+router.post("/", ensureAdmin, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, userNewSchema);
     if (!validator.valid) {
@@ -48,10 +52,11 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
  *
  * Returns list of all users.
  *
- * Authorization required: login
+ * Authorization required: admin
  **/
+//Getting the list of all users should only be permitted by admins.
 
-router.get("/", ensureLoggedIn, async function (req, res, next) {
+router.get("/", ensureAdmin, async function (req, res, next) {
   try {
     const users = await User.findAll();
     return res.json({ users });
@@ -67,8 +72,10 @@ router.get("/", ensureLoggedIn, async function (req, res, next) {
  *
  * Authorization required: login
  **/
+//Getting information on a user, updating, or deleting a user should only be permitted either by an admin, 
+//or by that user.
 
-router.get("/:username", ensureLoggedIn, async function (req, res, next) {
+router.get("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
   try {
     const user = await User.get(req.params.username);
     return res.json({ user });
@@ -87,10 +94,12 @@ router.get("/:username", ensureLoggedIn, async function (req, res, next) {
  *
  * Authorization required: login
  **/
+////Getting information on a user, updating, or deleting a user should only be permitted either by an admin, 
+//or by that user.
 
-router.patch("/:username", ensureLoggedIn, async function (req, res, next) {
+router.patch("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
   try {
-    const validator = jsonschema.validate(req.body, userUpdateSchema);
+    const validator = jsonschema.validate(req.body, userUpdateSchema);//validating the data that is being sent to the server.
     if (!validator.valid) {
       const errs = validator.errors.map(e => e.stack);
       throw new BadRequestError(errs);
@@ -108,8 +117,10 @@ router.patch("/:username", ensureLoggedIn, async function (req, res, next) {
  *
  * Authorization required: login
  **/
+////Getting information on a user, updating, or deleting a user should only be permitted either by an admin, 
+//or by that user.
 
-router.delete("/:username", ensureLoggedIn, async function (req, res, next) {
+router.delete("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
   try {
     await User.remove(req.params.username);
     return res.json({ deleted: req.params.username });
