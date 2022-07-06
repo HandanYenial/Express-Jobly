16939f5157298be7b62e34d9a038a8a864b8ecfe
Your Name <you@example.com>
2022-07-06 12:27:56 -0400

    adding .gitignore

--- models/company.test.js
+++ models/company.test.js
@@ -59,41 +59,39 @@ describe("create", function () {
 /************************************** findAll */
 
 describe("findAll", function () {
-
   test("works: all", async function () {
     let companies = await Company.findAll();
     expect(companies).toEqual([
       {
         handle: "c1",
-        name: "C1",
+        name: "Client1",
         description: "Desc1",
         numEmployees: 1,
         logoUrl: "http://c1.img",
       },
       {
         handle: "c2",
-        name: "C2",
+        name: "Client2",
         description: "Desc2",
         numEmployees: 2,
         logoUrl: "http://c2.img",
       },
       {
         handle: "c3",
-        name: "C3",
+        name: "Client3",
         description: "Desc3",
         numEmployees: 3,
         logoUrl: "http://c3.img",
       },
     ]);
   });
-
   test("works: by min employees" , async function(){
     let companies = await Company.findAll({ minEmployees :2 });
     expect(companies).toEqual([
       {
         handle : "c2",
         name   : "C2",
-        decription : "Desc2",
+        description : "Desc2",
         numEmployees : 2,
         logoUrl : "http://c2.img",
       },
